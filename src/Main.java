9c4b2bab97862dbd4db06251729d09efb402bc09
marbazalgette <marin.bazalgette@gmail.com>
2016-01-03 00:57:28 +0100

    petites améliorations dans le main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 //package pong;
 
 //import pong.gui.Window;
@@ -7,6 +10,7 @@
  * Starting point of the Pong application
  */
 public class Main  {
+	
 	
 	public static void main(String[] args) {
 		
@@ -19,10 +23,15 @@ public class Main  {
 		else {
 			Pong pong = new Pong();
 			Window window = new Window(pong);
-			String computerName;               
-			computerName = dialog.secondDialog();  // on récupère le nom de la machine de l'autre joueur
-			Network network = new Network();     // on créé un serveur socket
-			network.connection(computerName); // et un client
+			InetAddress player2Address = null;               
+			Network network = new Network();    
+			try {
+				player2Address.getByName(dialog.secondDialog());	
+				network.connexion(player2Address); 
+			}
+			catch (UnknownHostException e) {
+				//
+			}
 			Racket racketDroite = new Racket("img/racket.png",600,300,0);
 			pong.list.add(racketDroite);
 			
