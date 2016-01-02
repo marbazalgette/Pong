c6b96d58759fc28b2bda360f7c101092f17e0738
marbazalgette <marin.bazalgette@gmail.com>
2016-01-02 18:24:35 +0100

    l'idée principale est la

--- src/Communication.java
+++ src/Communication.java
@@ -9,7 +9,7 @@ public class Communication {
 	private OutputStream output;
 	private Network network;
 	private Communication communication;
-	private Racket r;
+	private Racket racket;
 	
 	public Communication(Socket s) {
 		try {
@@ -21,18 +21,18 @@ public class Communication {
 		}
 	}
 	
-	public void protocol(Racket r) {
-		communication.newPosition(r, output);
-		communication.updatePosition(input);	
+	public void protocol() {
+		this.communication.newPosition(racket, output);
+		this.communication.updatePosition(readStream(input));	
 	}
 	
 	public void newPosition(Racket r, OutputStream output) {
 		try {
 			StringBuffer strBuff = new StringBuffer();
 			strBuff.append(r.getPosX());
-			strBuff.append('-');
+			strBuff.append((int)1);
 			strBuff.append(r.getPosY());
-			strBuff.append('/');
+			strBuff.append((int)2);
 			output.write(strBuff.toString().getBytes());
 			output.flush();
 		}
@@ -41,7 +41,30 @@ public class Communication {
 		}
 	}
 	
-	public void updatePosition (InputStream input) {
-		
+	public void updatePosition (int[] intTab) {
+		racket.setPosX(intTab[0]);
+		racket.setPosX(intTab[1]);
 	}
+	
+
+	public int[] readStream(InputStream input) { //peur etre a refaire avec des strings
+		int [] intTab = new int[2];
+		int tmp = 0;
+		try {
+			while (tmp != 2) {
+				int cpt=0;
+				tmp = (int)input.read();
+				if (tmp != 1 && tmp != 2) {
+				intTab[cpt] = tmp;
+				cpt++;
+				}
+			}
+		}
+		catch (IOException e) {
+			//
+		}
+		return intTab;
+	}
+		
+		
 }
